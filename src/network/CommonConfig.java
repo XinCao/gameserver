49bb64ef846150750a17bd79ebcdf72b0a8cf12f
曹鑫 <jingxin191314@foxmail.com>
2013-12-11 09:35:17 +0800

    网络组建

---
+++ src/network/CommonConfig.java
@@ -0,0 +1,14 @@
+package network;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: colin
+ * Date: 12-9-22
+ * Time: 上午11:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class CommonConfig
+{
+	public static boolean  isTencentPlatform = false;
+	public static int gameserver_id = 0;
+}
