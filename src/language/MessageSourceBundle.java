f358f1945047049e7bc8078649634cf36bce0f08
曹鑫 <jingxin191314@foxmail.com>
2013-12-13 13:12:45 +0800

    使用更强大的spring messageSrouce

---
+++ src/language/MessageSourceBundle.java
@@ -0,0 +1,26 @@
+package language;
+
+import java.util.Locale;
+import org.springframework.context.MessageSource;
+
+/**
+ * 语言本地化（可以给属性文件传递参数）
+ *
+ * @author caoxin
+ */
+public class MessageSourceBundle {
+
+    private MessageSource messageSource;
+
+    public String getMessage(String key, Object[] o) {
+        return messageSource.getMessage(key, o, Locale.getDefault());
+    }
+
+    public MessageSource getMessageSource() {
+        return messageSource;
+    }
+
+    public void setMessageSource(MessageSource messageSource) {
+        this.messageSource = messageSource;
+    }
+}
