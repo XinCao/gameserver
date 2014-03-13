030195068c0f18432cd418dff311de474feccaee
曹鑫 <jingxin191314@foxmail.com>
2014-03-13 18:33:57 +0800

    ok

--- src/gameserver/GameServer.java
+++ src/gameserver/GameServer.java
@@ -2,7 +2,6 @@ package gameserver;
 
 import java.io.IOException;
 import gameserver.network.NioServer;
-import gameserver.service.TableService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.FileSystemXmlApplicationContext;
 
@@ -11,7 +10,5 @@ public class GameServer {
     public static void main(String... args) throws IOException {
         ApplicationContext ac = new FileSystemXmlApplicationContext("./config/app.xml");
         NioServer.startServer();
-        TableService tableService = ac.getBean(TableService.class);
-        tableService.getTable(TableService.TableKind.ITEM_TEMPLATE).getBaseTableRow(1);
     }
 }
---
+++ src/gameserver/service/AsyncHandlerService.java
@@ -0,0 +1,16 @@
+package gameserver.service;
+
+import org.springframework.scheduling.annotation.Async;
+
+/**
+ *
+ * 异步方法调用
+ * 
+ * @author caoxin
+ */
+public class AsyncHandlerService {
+
+    @Async
+    public void async_test() {
+    }
+}
